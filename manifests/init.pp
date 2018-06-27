f5d2d15bd98c67eba00a1801aaa68defe584ad19
Britt Gresham <britt@brittg.com>
2018-06-27 07:32:30 -0700

    Add a default remote to fix puppet issues

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,12 +3,13 @@ class dotfiles (
   String $homedir = "/home/${user}",
 ){
   vcsrepo { "${homedir}/.dotfiles":
-    ensure        => latest,
-    provider      => git,
-    user          => $user,
-    source        => {
-      'master'    => 'https://github.com/demophoon/dotfiles.git',
-      'demophoon' => 'git@github.com:demophoon/dotfiles.git',
+    ensure              => latest,
+    provider            => git,
+    user                => $user,
+    remote              => 'demophoon-https',
+    source              => {
+      'demophoon-https' => 'https://github.com/demophoon/dotfiles.git',
+      'demophoon'       => 'git@github.com:demophoon/dotfiles.git',
     },
     notify => Exec['Install Dotfiles'],
   }
